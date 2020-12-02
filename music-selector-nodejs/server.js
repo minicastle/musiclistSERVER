8e13c4e0cd0f6333496c820680c98fb48dd50eda
unknown <jk598200167@gmail.com>
2020-12-02 16:25:10 +0900

    CRUD

--- music-selector-nodejs/server.js
+++ music-selector-nodejs/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const url = require("url");
+const qs = require("querystring");
 const app = express();
-const port = process.env.PORT||5000;
 const mysql = require("mysql");
 const db = mysql.createConnection({
     host:"localhost",
@@ -15,20 +16,66 @@ db.connect();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
-app.get("/musiclist/arlist",function(req,res){
-    db.query(`SELECT * FROM arlist`,function(err,arlist){
-        if(err)throw err;
-        res.send(arlist);
-    });
+app.listen(5000,function(){
+    console.log(`Listening on port 5000`);
 });
 
-app.get("/musiclist/mrlist",function(req,res){
-    db.query(`SELECT * FROM mrlist`,function(err,mrlist){
-        if(err)throw err;
-        res.send(mrlist);
+    app.get("/musiclist/arlist",function(req,res){
+        db.query(`SELECT * FROM arlist`,function(err,arlist){
+            if(err)throw err;
+            res.send(arlist);
+        });
     });
-});
 
-app.listen(port,function(){
-    console.log(`Listening on port ?`,[port]);
-})
+    app.get("/musiclist/mrlist",function(req,res){
+        db.query(`SELECT * FROM mrlist`,function(err,mrlist){
+            if(err)throw err;
+            res.send(mrlist);
+        });
+    });
+
+    app.post("/musiclist/create_process",function(req,res){
+    if(req.body.list==="ArList"){
+        db.query(`INSERT INTO arlist(arlist_title,arlist_singer,arlist_link,arlist_author_id,arlist_created) VALUES(?,?,?,?,NOW())`,[req.body.title,req.body.singer,req.body.Url,1],function(err,result){
+            if(err)throw err;
+            res.writeHead(302,{Location:`http://localhost:3000/`});
+            res.end();
+        });
+    }
+    if(req.body.list === "Mrist"){
+        db.query(`INSERT INTO mrlist(mrlist_title,mrlist_singer,mrlist_link,mrlist_author_id,mrlist_created) VALUES(?,?,?,?,NOW())`,[req.body.title,req.body.singer,req.body.Url,1],function(err,result){
+            if(err)throw err;
+            res.writeHead(302,{Location:`http://localhost:3000/`});
+            res.end();
+        });
+    }
+    });
+
+    app.post("/musiclist/delete_process_arlist",function(req,res){
+        db.query(`DELETE FROM arlist WHERE arlist_id=?`,[req.body.id],function(err,result){
+            if(err)throw err;
+            res.writeHead(302,{Location:`http://localhost:3000/`})
+            res.end();
+        });
+    });
+    app.post("/musiclist/delete_process_mrlist",function(req,res){
+        db.query(`DELETE FROM mrlist WHERE mrlist_id=?`,[req.body.id],function(err,result){
+            if(err)throw err;
+            res.writeHead(302,{Location:`http://localhost:3000/`})
+            res.end();
+        });
+    });
+    app.post("/musiclist/update_process_arlist",function(req,res){
+        db.query(`UPDATE arlist SET arlist_title = ?,arlist_link = ? WHERE arlist_id = ?`,[req.body.title,req.body.url,req.body.id],function(err,result){
+            if(err)throw err;
+            res.writeHead(302,{Location:`http://localhost:3000/`})
+            res.end();
+        })
+    });
+    app.post("/musiclist/update_process_mrlist",function(req,res){
+        db.query(`UPDATE mrlist SET mrlist_title = ?,mrlist_link = ? WHERE mrlist_id = ?`,[req.body.title,req.body.url,req.body.id],function(err,result){
+            if(err)throw err;
+            res.writeHead(302,{Location:`http://localhost:3000/`})
+            res.end();
+        })
+    });
